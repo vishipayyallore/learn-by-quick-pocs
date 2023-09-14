2ec269a4f1f4cf3d66cf9f6ec3be4801248e1fc0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-14 10:34:34 +0530

    Initial Version

---
+++ GRecaptchaV3Demo/GRecaptchaV3Demo/Site.Mobile.Master.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GRecaptchaV3Demo
+{
+    public partial class Site_Mobile : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
