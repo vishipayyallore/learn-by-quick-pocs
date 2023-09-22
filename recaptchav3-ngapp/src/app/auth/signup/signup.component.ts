2bd7f84ba0ab6974318f6597a5a007193e3c6b7f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-22 14:44:22 +0530

    Sign up Form

--- recaptchav3-ngapp/src/app/auth/signup/signup.component.ts
+++ recaptchav3-ngapp/src/app/auth/signup/signup.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
   selector: 'sv-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  token: string = '';
+
+  constructor(private recaptchaV3Service: ReCaptchaV3Service) {
+  }
+
+  ngOnInit() {
+    console.log('App component initialized!');
+
+    this.recaptchaV3Service.execute('importantAction')
+      .subscribe((token: string) => {
+        this.token = token;
+        console.log(`Token [${token}] generated`);
+      });
+  }
 
 }
