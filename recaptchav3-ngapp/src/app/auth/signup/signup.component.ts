739bd89404d77202f1b59c2f8c772b98cc5f0dab
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-26 16:07:03 +0530

    Changes

--- recaptchav3-ngapp/src/app/auth/signup/signup.component.ts
+++ recaptchav3-ngapp/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // Import HttpClient
 import { ReCaptchaV3Service } from 'ng-recaptcha';
 import RecaptchaResponse from '../../interfaces/RecaptchaResponse'; // Import the RecaptchaResponse interface
@@ -11,8 +11,10 @@ import RecaptchaResponse from '../../interfaces/RecaptchaResponse'; // Import th
 export class SignupComponent implements OnInit {
   token: string = '';
   formData: any = {}; // Initialize an empty object for form data
+  success: boolean | undefined; // Add a property for success
+  score: number | undefined;    // Add a property for score
 
-  constructor(private recaptchaV3Service: ReCaptchaV3Service, private http: HttpClient) {
+  constructor(private recaptchaV3Service: ReCaptchaV3Service, private http: HttpClient, private cdr: ChangeDetectorRef) {
   }
 
   ngOnInit() {
@@ -32,16 +34,31 @@ export class SignupComponent implements OnInit {
 
     // Send the form data including the token to your /register endpoint
     // Replace 'your_api_endpoint' with the actual API endpoint URL
-    this.http.post<RecaptchaResponse>('https://localhost:7199/signup', this.formData)
-      .subscribe((response) => {
-        console.log(`POST response: ${response}`);
-        const parsedResponse: RecaptchaResponse = response;
-        console.log('Registration successful', response);
+    this.http.post('https://localhost:7199/signup', this.formData)
+      .subscribe((response: any) => {
+        console.log('POST response:', response);
+        const parsedResponse: RecaptchaResponse = response as RecaptchaResponse;
+        console.log('Registration successful', response, parsedResponse.data.score);
+
+        // Remove newline characters from the data property
+        const parsedData = JSON.parse(response.data.replace(/\n/g, ''));
+
+        // Now, parsedData contains the JSON object without newline characters
+        console.log(parsedData);
+
+        // Access the success and score properties from parsedData
+        this.success = parsedData.success;
+        this.score = parsedData.score;
+
+        // Manually trigger change detection
+        // this.cdr.detectChanges();
+
         // Handle success response here
       }, (error) => {
         console.error('Registration failed', error);
         // Handle error response here
       });
+
   }
 
 }
