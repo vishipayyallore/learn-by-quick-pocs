d9c768ce96d635bb8cc01e4f40eb4093602b76c9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-08 18:52:51 +0530

    Update recaptcha-token.service.ts

--- recaptchav3-ngapp/src/app/services/recaptcha-token.service.ts
+++ recaptchav3-ngapp/src/app/services/recaptcha-token.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
-import { Subject, first } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecaptchaTokenService {
 
-  token$ = new Subject<string>();
-
   constructor(private recaptchaV3Service: ReCaptchaV3Service) {
   }
 
-  getRecaptchaToken() {
-    this.recaptchaV3Service.execute('importantAction')
-      .pipe(first())
-      .subscribe((token) => {
-        console.log(`getRecaptchaToken() :: Token [${token}] generated at ${new Date().toTimeString()}`);
-        this.token$.next(token);
-      });
+  getRecaptchaToken(): Observable<string> {
+    return this.recaptchaV3Service.execute('importantAction');
   }
 
 }
